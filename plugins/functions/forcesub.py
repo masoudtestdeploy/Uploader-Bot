8172c6f20e34f2f29ab4c32096cfcd5d54cf001a
masoudtestdeploy <95133886+masoudtestdeploy@users.noreply.github.com>
2022-02-19 23:27:18 +0330

    Update forcesub.py

--- plugins/functions/forcesub.py
+++ plugins/functions/forcesub.py
@@ -15,7 +15,7 @@ async def handle_force_subscribe(bot, message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=message.from_user.id,
-                text="Sorry Sir, You are Banned. Contact My [Support Group](https://t.me/tellybots_4u).",
+                text="Sorry Sir, You are Banned. Contact My [Support Group](https://t.me/KenzoMovie).",
                 parse_mode="markdown",
                 disable_web_page_preview=True,
                 reply_to_message_id=message.message_id,
@@ -39,7 +39,7 @@ async def handle_force_subscribe(bot, message):
     except Exception:
         await bot.send_message(
             chat_id=message.from_user.id,
-            text="Something Went Wrong. Contact My [Support Group](https://t.me/Tellybots_4u).",
+            text="Something Went Wrong. Contact My [Support Group](https://t.me/KenzoMovie).",
             parse_mode="markdown",
             disable_web_page_preview=True,
             reply_to_message_id=message.message_id,
